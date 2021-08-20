6a42e5b793fea7ec4a7a08bcc46df11562381863
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2021-08-20 13:48:54 -0500

    first commit

---
+++ src/build/precache-manifest.42e991ec1b8ff461131a0d1d26617f0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2da040a471c10dbb63b4",
+    "url": "/static/css/main.ac0e2d76.chunk.css"
+  },
+  {
+    "revision": "2da040a471c10dbb63b4",
+    "url": "/static/js/main.85c27ac4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1ca2a6b433f45652b1c4",
+    "url": "/static/js/2.ed1a1ba2.chunk.js"
+  },
+  {
+    "revision": "507adbb9d9627870786997ad08d824b5",
+    "url": "/index.html"
+  }
+];
